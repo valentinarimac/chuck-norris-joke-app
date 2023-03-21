dded4f4bbd5a2a52a8aeb48e9c16a5034b559249
Valentina Rimac <valetinaarimac@gmail.comgit config --global user.email valetinaarimac@gmail.comgit config --global user.email valetinaarimac@gmail.com>
2023-03-21 02:00:18 +0100

    Add params file

---
+++ src/params.ts
@@ -0,0 +1,7 @@
+const SECRET = "bird987";
+const MONGO_URL =
+  "mongodb+srv://valentina:joke123@cluster0.9h9njv5.mongodb.net/chuck-norris-joke-app?retryWrites=true&w=majority";
+const EMAIL = "tt5396992@gmail.com";
+const PWD = "ojsoodtosllxalyf";
+
+export { SECRET, MONGO_URL, EMAIL, PWD };
